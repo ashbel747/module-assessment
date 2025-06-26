d1d839ffe8e15a9109e064d363a379b56db3f256
Ashbel <kashbel747@gmail.com>
2025-06-26 19:50:25 +0300

    Refactor: Added typing animation

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ setInterval(() => {
     index = (index + 1) % slides.length;
     carousel.style.transform = `translateX(-${index * 100}%)`;
   }, 8000);
+
+const text = "Paradiso Afrique";
+  const typeanimation = document.getElementById("typeWriter");
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      typeanimation.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 150);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", typeWriter);
